b08fe16d1fbfcaa39d4ac2b7cc397f0654ef1faf
yahya6-dev <yahyasaid9350@gmail.com>
2022-03-19 20:09:08 +0100

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager
+from flask_pagedown import PageDown
+from config import config
+
+##all of the required functionality we instantiate them here
+login_manager = LoginManager()
+bootstrap = Bootstrap5()
+mail = Mail()
+moment = Moment()
+pagedown = PageDown()
+db = SQLAlchemy()
+
+login_manager.login_view = "auth.login"
+##create and initialize app
+def create_app(config_name):
+	app = Flask(__name__)
+	app.config.from_object(config[config_name])
+
+	config[config_name].init_app(app)
+
+	mail.init_app(app)
+	login_manager.init_app(app)
+	bootstrap.init_app(app)
+	moment.init_app(app)
+	pagedown.init_app(app)
+	db.init_app(app)
+
+	return app
+
---
+++ config.py
@@ -0,0 +1,53 @@
+import os
+import uuid
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+##main configuration class for the app
+##configuring mail configuration
+##database configuration
+class Config:
+	MAIL_SERVER = "smtp.gmail.com"
+	MAIL_PORT = 578
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME","yahyasaid935@gmail.com")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+	MAIL_USE_TLS = True
+	SQLALCHEMY_RECORD_QUERIES = True
+	SECRET_KEY = uuid.uuid4().hex
+	SQLALCHEMY_TRACK_MODIFICSTIONS = False
+
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+
+##Developement configuration to use derived from Config
+##and speciliaze it with custom fields
+##including set application logger
+class DevelopmentConfig(Config):
+	SQLALCHEMY_DATABASE_URIv= os.getenv("DEV_DATABASE") or "sqlite:///"+os.path.join(basedir,"dev_database.sqlite")
+
+	@staticmethod
+	def init_app():
+		import logging
+		from logging import StreamHandler
+		handler = StreamHandler()
+		handler.setLevel(logging.DEBUG)
+		app.logger.addHandler(handler)
+
+
+
+##main config for running 
+#no logger is needed here
+class TestingConfig(Config):
+	SQLALCHEMY_DATABASE_URI = os.env.get("TEST_DEV") or "sqlite:///"+ os.path.join(basedir,"test_database.sqlite")
+	WTF_CSRF_ENABLED = False
+
+
+#all config
+config = {
+		"developement":DevelopementConfig,
+		"testing":TestingConfig
+
+	}
